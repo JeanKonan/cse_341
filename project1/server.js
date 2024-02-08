93235efc9dcb527921b167e8026c03b1aa83352f
JeanKonan <kon22005@byui.edu>
2024-02-08 13:50:39 -0700

    swagger documentation added

--- project1/server.js
+++ project1/server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const swaggerAutogen = require('swagger-autogen')();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger-output.json');
 const myConnection = require('./db/connect');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,6 +10,7 @@ app.use(express.json());
 console.log('Starting server...');
 app.use('/', require('./routes'));
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 myConnection.initDb((err) => {
   if (err) {
