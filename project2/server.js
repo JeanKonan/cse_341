63cff935b7d1bfdef749bc3b8413807f9b4ccee4
JeanKonan <kon22005@byui.edu>
2024-02-24 01:56:51 -0700

    Auth0 added

--- project2/server.js
+++ project2/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongodb = require('./db/connect');
+const dotenv = require('dotenv');
+const { auth, requiresAuth} = require('express-openid-connect');
+dotenv.config();
 const app = express();
 const port = process.env.port || 3000;
 
@@ -13,6 +16,25 @@ app.use('/', require('./routes'));
 
 app.use('/api-docs', swaggerUi.serve);
 app.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: process.env.ISSUER_BASE_URL,
+  };
+
+app.use(auth(config));
+
+app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+  
+app.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+});
 
 mongodb.initDb((err) => {
     if (err) {
