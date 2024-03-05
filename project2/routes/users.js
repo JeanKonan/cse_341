b3e58418080919b5c7388bb018360ee7c80dbeb4
JeanKonan <kon22005@byui.edu>
2024-03-05 13:31:46 -0700

    requiresAuth added to get

--- project2/routes/users.js
+++ project2/routes/users.js
@@ -1,11 +1,12 @@
 const { getAll, getSingle, createUser, updateUser, deleteUser } = require('../controllers/users');
 const { idRules, validationRules } = require('../middleware/user_validator');
+const { auth, requiresAuth} = require('express-openid-connect');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', getAll);
+router.get('/', requiresAuth(), getAll);
 
-router.get('/:id', idRules(), getSingle);
+router.get('/:id', requiresAuth(), idRules(), getSingle);
 
 router.post('/', validationRules(), createUser);
 
